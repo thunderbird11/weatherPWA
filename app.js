1794d311d7f659aec838a5f10e28b2f905365ad1
kenchen <kenchen.wow@gmail.com>
2019-09-15 23:23:27 +0800

    code structure

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './')))
+app.get('*', function(req, res) {
+  const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
+  res.send(html)
+})
+
+app.listen(8088, function() {
+  console.log('server listening on port 8088!')
+})
